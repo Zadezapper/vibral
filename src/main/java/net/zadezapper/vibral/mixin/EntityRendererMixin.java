f546807826880b8ca334eb540da7397aa6b63800
Zadezapper <zadezapper@gmail.com>
2026-05-25 10:05:12 -0600

    + Attempt at making vibral panels and blocks occlude nametags

--- src/main/java/net/zadezapper/vibral/mixin/EntityRendererMixin.java
+++ src/main/java/net/zadezapper/vibral/mixin/EntityRendererMixin.java
@@ -1,6 +1,9 @@
 package net.zadezapper.vibral.mixin;
 
 
+import net.minecraft.block.BlockState;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
@@ -8,12 +11,21 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.RaycastContext;
 import net.zadezapper.vibral.item.ModItems;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static net.zadezapper.vibral.block.ModBlocks.VIBRAL_PANEL;
+import static net.zadezapper.vibral.block.ModBlocks.VIBRAL_BLOCK;
 
 @Mixin(value = EntityRenderer.class, priority = 2048)
 public abstract class EntityRendererMixin<entity extends Entity> {
@@ -24,6 +36,35 @@ public abstract class EntityRendererMixin<entity extends Entity> {
             if (isWearingFullVibralArmorSet(entity)) {
                 callbackInfo.cancel();
             }
+        }
+    }
+
+    @Inject(method = "renderLabelIfPresent", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;isSneaky()Z"), cancellable = true)
+    private void renderLabelIfPresent(Entity entity, Text text, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, float tickDelta, CallbackInfo callbackInfo) {
+        Camera camera = MinecraftClient.getInstance().gameRenderer.getCamera();
+        // Goal: Raycast from the camera to the entity, and check for collisions with vibral blocks. If collision found, cancel nametag rendering.
+        // Currently: Appears to raycast from camera to player with too large step size, and or cuts off at some point for some reason
+        Vec3d cameraPosition = camera.getPos();
+        Vec3d entityPosition = entity.getBoundingBox().getCenter(); // entity.getCameraPosVec(tickDelta);
+        Vec3d rayVector = cameraPosition.subtract(entityPosition); // entityPosition.add(entityRotation.x * distance, entityRotation.y * distance, entityRotation.z * distance); // Vector starting at entity and pointing where entity is looking
+        RaycastContext context = new RaycastContext(
+                entityPosition, rayVector, RaycastContext.ShapeType.OUTLINE, RaycastContext.FluidHandling.NONE, entity
+        );
+        BlockHitResult hit = BlockView.raycast(context.getStart(), context.getEnd(), context, (innerContext, pos) -> {
+            BlockState blockState = entity.getWorld().getBlockState(pos);
+
+            if (blockState.getBlock() != VIBRAL_PANEL && blockState.getBlock() != VIBRAL_BLOCK) {
+                return null;
+            }
+
+            Vec3d vec3d4 = innerContext.getStart();
+            Vec3d vec3d5 = innerContext.getEnd();
+            VoxelShape voxelShape = innerContext.getBlockShape(blockState, entity.getWorld(), pos);
+            return entity.getWorld().raycastBlock(vec3d4, vec3d5, pos, voxelShape, blockState);
+        }, innerContext -> null);
+
+        if (hit != null) {
+            callbackInfo.cancel();
         }
     }
 
